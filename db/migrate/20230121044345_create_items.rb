00933742d0a36767ce12fb2a48fcc7d00c1a98bd
Newburu <newburu@gmail.com>
2023-01-21 13:46:11 +0900

    add Item

---
+++ db/migrate/20230121044345_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.references :product, null: false, foreign_key: true
+      t.string :name
+      t.date :release_at
+
+      t.timestamps
+    end
+  end
+end

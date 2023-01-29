fabbf8d10ea67e320781cc0935e2b30a9798983f
Newburu <newburu@gmail.com>
2023-01-29 22:30:33 +0900

    初期データを複数登録

--- db/fixtures/development/02_brand.rb
+++ db/fixtures/development/02_brand.rb
@@ -1,5 +1,6 @@
-Brand.seed do |s|
-  s.id = 1
-  s.store_id = 1
-  s.name = 'ブランドA'
-end
+Brand.seed(:id,
+  { id: 1, store_id: 1, name: 'ブランドA-1' },
+  { id: 2, store_id: 1, name: 'ブランドA-2' },
+  { id: 3, store_id: 2, name: 'ブランドB-1' },
+  { id: 4, store_id: 2, name: 'ブランドB-2' },
+)

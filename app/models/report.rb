630b44fad5b3e95337a6c453b856bceee2e13d36
Newburu <newburu@gmail.com>
2023-03-19 15:12:10 +0900

    評価を登録できるようにする Fixes #26

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,8 @@
 class Report < ApplicationRecord
+  has_many :report_assessments
+  has_many :assessments, through: :report_assessments
+  accepts_nested_attributes_for :report_assessments
+
   belongs_to :item
   mount_uploaders :images, ImageUploader
   serialize :images
---
+++ db/fixtures/development/05_assessment.rb
@@ -0,0 +1,6 @@
+Assessment.seed(:id,
+  { id: 1, name: '総合' },
+  { id: 2, name: '味' },
+  { id: 3, name: '見た目' },
+  { id: 4, name: 'コスパ' },
+)

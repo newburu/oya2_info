e68935e3a5020affb382bf9e2601de813d2d3d5a
Newburu <newburu@gmail.com>
2023-04-10 23:41:03 +0900

    403エラー画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    render file: Rails.root.join('public/403.html'), status: :forbidden
+  end
+
 end

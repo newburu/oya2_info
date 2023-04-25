1599d359ddaf49d928becbe47c2d802a4c4ae87a
Newburu <newburu@gmail.com>
2023-04-25 23:55:03 +0900

    ユーザアイコンをTwitterアイコンから取得

--- db/schema.rb
+++ db/schema.rb
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_145542) do
     t.string "password"
     t.string "access_token"
     t.string "access_token_secret"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
